c2b42e5fef13dabfe95a97a02a05574475317ac7
culinera <vern.proxy@gmail.com>
2025-05-10 00:39:00 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,22 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// This file assumes you have already loaded the Firebase compat scripts in your HTML:
+// <script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
+// <script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js"></script>
+// <script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js"></script>
+// (add storage-compat.js, analytics-compat.js, etc. if you use those features)
+
 const firebaseConfig = {
   apiKey: "AIzaSyCMi4S-JrwqT8DLMYjUvPMUgOZ_f02EDeI",
   authDomain: "culinera-recipes.firebaseapp.com",
   projectId: "culinera-recipes",
-  storageBucket: "culinera-recipes.firebasestorage.app",
-  messagingSenderId: "642219261914",
-  appId: "1:642219261914:web:f621b8581a4f0791694ae3",
-  measurementId: "G-LJTE9X1C99"
+  storageBucket: "culinera-recipes.appspot.com",
+  messagingSenderId: "64221926914",
+  appId: "1:64221926914:web:f621b8581a4f0791694ae3",
+  measurementId: "G-LJTEX9XC99"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Only initialize once, even if this file is loaded multiple times
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
