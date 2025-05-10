4ead674bff9cf1a7fc2fdb228cf8f6f20d12725e
culinera <vern.proxy@gmail.com>
2025-05-09 22:13:40 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase configuration loaded from environment variables
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCfA_-PJU1pb3qDMWuHk-sl0H85dpOJS40",
-  authDomain: "vernecipe.firebaseapp.com",
-  projectId: "vernecipe",
-  storageBucket: "vernecipe.firebasestorage.app",
-  messagingSenderId: "317986656278",
-  appId: "1:317986656278:web:034899fc23050432cb2537",
-  measurementId: "G-XBSPY8BW68"
+  apiKey: "AIzaSyCMi4S-JrwqT8DLMYjUvPMUgOZ_f02EDeI",
+  authDomain: "culinera-recipes.firebaseapp.com",
+  projectId: "culinera-recipes",
+  storageBucket: "culinera-recipes.firebasestorage.app",
+  messagingSenderId: "642219261914",
+  appId: "1:642219261914:web:f621b8581a4f0791694ae3",
+  measurementId: "G-LJTE9X1C99"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-export { app, analytics };
