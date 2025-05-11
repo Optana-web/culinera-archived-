5b127519bc2db458a071197fb2b8842416267a63
Optana-web <vern.proxy@gmail.com>
2025-05-10 20:33:21 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,19 @@
 // firebase-config.js
 
 // This file assumes you have already loaded the Firebase compat scripts in your HTML:
-// <script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
-// <script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js"></script>
-// <script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js"></script>
 // (add storage-compat.js, analytics-compat.js, etc. if you use those features)
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMi4S-JrwqT8DLMYjUvPMUgOZ_f02EDeI",
-  authDomain: "culinera-recipes.firebaseapp.com",
-  projectId: "culinera-recipes",
-  storageBucket: "culinera-recipes.appspot.com",
-  messagingSenderId: "64221926914",
-  appId: "1:64221926914:web:f621b8581a4f0791694ae3",
-  measurementId: "G-LJTEX9XC99"
+  apiKey: "AIzaSyA_CZn_fLuzyBoWCqKxI0SHPalTGBaLIGc",
+  authDomain: "optanav1.firebaseapp.com",
+  projectId: "optanav1",
+  storageBucket: "optanav1.firebasestorage.app",
+  messagingSenderId: "16298669202",
+  appId: "1:16298669202:web:ac2a1cd399993b1883624b",
+  measurementId: "G-RR7D4MB5FY"
 };
 
 // Only initialize once, even if this file is loaded multiple times
